e957ae4dd9eda401a524ceb625aca1f3f54491ab
Stephen White <iamsilk3495@gmail.com>
2021-06-04 16:23:45 -0300

    Embed config file and export at runtime

--- Drop/Program.cs
+++ Drop/Program.cs
@@ -1,12 +1,39 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+using System.Resources;
 
 namespace Drop
 {
     public class Program
     {
+        private static void ExportResource(string resource)
+        {
+            var resourcePath = Path.Combine(Environment.CurrentDirectory, resource);
+
+            if (File.Exists(resourcePath)) return;
+
+            var assembly = typeof(Program).Assembly;
+
+            using var stream = assembly.GetManifestResourceStream($"{assembly.GetName().Name}.{resource}");
+            using var reader = new StreamReader(stream ?? throw new MissingManifestResourceException("Missing embedded resource"));
+
+            var contents = reader.ReadToEnd();
+
+            File.WriteAllText(resourcePath, contents);
+        }
+
         public static void Main(string[] args)
         {
+#if DEBUG
+            var configResource = "appsettings.Development.json";
+#else
+            var configResource = "appsettings.json";
+#endif
+
+            ExportResource(configResource);
+
             CreateHostBuilder(args).Build().Run();
         }
 
